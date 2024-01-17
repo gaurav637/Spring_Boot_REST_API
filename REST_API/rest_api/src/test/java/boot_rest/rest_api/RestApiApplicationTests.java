efead74bd38d08e430afee0004634e3d556a8d7d
Gaurav Negi <141955844+gaurav637@users.noreply.github.com>
2024-01-17 16:04:01 +0530

    Add files via upload

---
+++ REST_API/rest_api/src/test/java/boot_rest/rest_api/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package boot_rest.rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
