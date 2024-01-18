121b514bec9d87f89253ccea8126b0df96cc2169
GauravNegi <negigaurav637@gmail.com>
2024-01-18 23:51:55 +0530

    add books list

--- REST_API/rest_api/src/main/java/boot_rest/rest_api/Controller/BookController.java
+++ REST_API/rest_api/src/main/java/boot_rest/rest_api/Controller/BookController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import boot_rest.rest_api.Entities.Book;
 
 // @Controller
 @RestController
@@ -14,8 +15,14 @@ public class BookController {
    // @RequestMapping(value="/books" , method= RequestMethod.GET)
     //@ResponseBody
     @GetMapping("/books")
-    public String getBooks(){
-        return "this is testing of books";
+    public Book getBooks(){
+
+        Book book = new Book();
+        book.setId(101);
+        book.setTitle("java");
+        book.setAuthor("Balaguruswami");
+
+        return book;
     }
     
 }
