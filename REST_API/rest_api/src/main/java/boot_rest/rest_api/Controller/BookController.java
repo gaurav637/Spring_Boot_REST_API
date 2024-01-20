6404f2886a220d36ed2949c4e16e13fa841c508f
GauravNegi <negigaurav637@gmail.com>
2024-01-20 16:01:14 +0530

    add 204 NO CONTENT

--- REST_API/rest_api/src/main/java/boot_rest/rest_api/Controller/BookController.java
+++ REST_API/rest_api/src/main/java/boot_rest/rest_api/Controller/BookController.java
@@ -27,6 +27,8 @@ public class BookController {
     @Autowired
     private BookServices bookServices;
 
+
+
    // @RequestMapping(value="/books" , method= RequestMethod.GET)
     //@ResponseBody
     // get all books 
@@ -45,41 +47,69 @@ public class BookController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
         return ResponseEntity.of(Optional.of(ls1));
-        
-
-        
     }
-
-
-
-
-
-
-
-
-
-
 
 
 
     // get single books
-    @GetMapping("/books/{id}")// RequestMapping(value="/value/{id}",method=MappingMethod.GET);
-    public Book getBook(@PathVariable("id") int id1){
-       return bookServices.getBookById(id1);
+    // @GetMapping("/books/{id}")// RequestMapping(value="/value/{id}",method=MappingMethod.GET);
+    // public Book getBook(@PathVariable("id") int id1){
+    //    return bookServices.getBookById(id1);
+    // }
+
+  /// create single book not found ResponseEntity 404 
+
+
+    @GetMapping("/books{booksid}")
+    public ResponseEntity<Book> getBook(@PathVariable("booksId") int id1){
+        Book b1  = bookServices.getBookById(id1);
+        if(b1==null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.of(Optional.of(b1));
     }
+  
+
 
     // add new books means add new data 
-    @PostMapping("/books")// @ReuestMapping(value="/books",method=RequestMethod.POST);
-    public Book addBook1(@RequestBody Book book){
-        Book b = bookServices.addBook(book);
-        return b;
+    // @PostMapping("/books")// @ReuestMapping(value="/books",method=RequestMethod.POST);
+    // public Book addBook1(@RequestBody Book book){
+    //     Book b = bookServices.addBook(book);
+    //     return b;
+    // }
+
+    // hander post  exception 
+
+    @PostMapping("/books")
+    public ResponseEntity<Book> addBook1(@RequestBody Book book){
+       Book b = null;
+       try{
+        b = bookServices.addBook(book);
+        return ResponseEntity.of(Optional.of(b));
+       }catch(Exception e){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+       }
     }
+
+
 
     // DELETE BOOKS HANDLER
 
-    @DeleteMapping("/books/{booksId}")// @RequestMapping(value="/value",method=RequestMethod.DELETE);
-    public void deleteBook1(@PathVariable("booksId") int id){
-        this.bookServices.deleteBook(id);
+    // @DeleteMapping("/books/{booksId}")// @RequestMapping(value="/value",method=RequestMethod.DELETE);
+    // public void deleteBook1(@PathVariable("booksId") int id){
+    //     this.bookServices.deleteBook(id);
+    // }
+
+    // Hander DELETE EXCEPTION
+
+    @DeleteMapping("/books/{booksId}")
+    public ResponseEntity<Void> deleteBook1(@PathVariable("booksId") int id){
+        try{
+            this.bookServices.deleteBook(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
 
--- REST_API/rest_api/src/main/java/boot_rest/rest_api/Services/BookServices.java
+++ REST_API/rest_api/src/main/java/boot_rest/rest_api/Services/BookServices.java
@@ -13,15 +13,15 @@ public class BookServices {
 
     private static List<Book> ls = new ArrayList<>();
     static{
-        // ls.add(new Book(12,"python","guido vanson"));
-        // ls.add(new Book(13,"let us c","denish ritchi"));
-        // ls.add(new Book(34,"java","sunmicrosystem"));
-        // ls.add(new Book(1,"pcat","xyz1"));
-        // ls.add(new Book(2,"os","xyz2"));
-        // ls.add(new Book(3,"c++","xyz3"));
-        // ls.add(new Book(4,"js","xyz4"));
-        // ls.add(new Book(5,"html","xyz5"));
-        // ls.add(new Book(6,"css","xyz6"));
+        ls.add(new Book(12,"python","guido vanson"));
+        ls.add(new Book(13,"let us c","denish ritchi"));
+        ls.add(new Book(34,"java","sunmicrosystem"));
+        ls.add(new Book(1,"pcat","xyz1"));
+        ls.add(new Book(2,"os","xyz2"));
+        ls.add(new Book(3,"c++","xyz3"));
+        ls.add(new Book(4,"js","xyz4"));
+        ls.add(new Book(5,"html","xyz5"));
+        ls.add(new Book(6,"css","xyz6"));
     }
 
     // get all books
@@ -34,7 +34,12 @@ public class BookServices {
 
     public Book getBookById(int id){
         Book book = null;
-        book = ls.stream().filter(e-> e.getId()==id).findFirst().get();
+        try{
+            book = ls.stream().filter(e-> e.getId()==id).findFirst().get();
+        }catch(Exception e){
+            System.out.println(e.getMessage());// printStackTrace(e);
+        }
+        
         return book;
     }
 
